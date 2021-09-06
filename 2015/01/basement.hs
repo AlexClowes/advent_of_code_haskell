2a3549b8799b84f98fec9c19fea1559c8b35d198
Alex Clowes <alexclowes@gmail.com>
2021-09-06 19:27:37 +0100

    Use fromJust to avoid printing Just in result

--- 2015/01/basement.hs
+++ 2015/01/basement.hs
@@ -1,8 +1,9 @@
 import Data.List
+import Data.Maybe
 import System.IO
 
-findBasement :: String -> Maybe Int
-findBasement brackets = elemIndex (-1) runningSum
+findBasement :: String -> Int
+findBasement brackets = fromJust $ elemIndex (-1) runningSum
     where runningSum = scanl (+) 0 $ map (\x -> if (x == '(') then 1 else -1) brackets
 
 main = do
--- 2015/04/hash.hs
+++ 2015/04/hash.hs
@@ -1,8 +1,9 @@
 import Data.Hash.MD5
 import Data.List
+import Data.Maybe
 
 
 main = do
     let hashes = map (md5s . Str . ("bgvyzdsv"++) . show) [0..]
-    print . findIndex (isPrefixOf "00000") $ hashes
-    print . findIndex (isPrefixOf "000000") $ hashes
+    print . fromJust . findIndex (isPrefixOf "00000") $ hashes
+    print . fromJust . findIndex (isPrefixOf "000000") $ hashes
