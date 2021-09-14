ec7a8a194a81b2545dbe557eafaf3d08c395af67
Alex Clowes <alexclowes@gmail.com>
2021-09-14 22:23:11 +0100

    2015 Problem 12

---
+++ 2015/12/ignore_red.hs
@@ -0,0 +1,19 @@
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import qualified Data.HashMap.Strict as M
+import Data.Maybe
+import qualified Data.Scientific as S
+import qualified Data.Text as T
+import qualified Data.Vector as V
+
+flatten :: Value -> [Int]
+flatten (Object obj) = if elem (String $ T.pack "red") (M.elems obj)
+                       then []
+                       else concat . map flatten $ M.elems obj
+flatten (Array arr) = concat . map flatten $ V.toList arr
+flatten (Number num) = [fromJust . S.toBoundedInteger $ num]
+flatten _ = []
+
+main = do
+    json <- B.readFile "input.json"
+    print . sum . flatten . fromJust $ (decode json :: Maybe Value)
